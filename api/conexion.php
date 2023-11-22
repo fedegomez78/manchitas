f6c81277ccb4bf9c6c44f0057e5df7d71021ba73
Arbinyey <arbinyey@icloud.com>
2023-11-22 08:18:15 -0500

    codigos php

--- api/conexion.php
+++ api/conexion.php
@@ -8,6 +8,40 @@ class Conexion{
 
     public function __construct(){
         //auto conexion
+        $this->host =  "localhost";
+        $this->user =  "root";
+        $this->pass =  "";
+        $this->db_name =  "manchitasdb";
+        $this->con = new mysqli ($this->host,$this->user,$this->db_name,$this->db_name);
+        $this->con->set_charset("utf8");
+        if($this->con->connect_error){
+            echo "Error al conectar a la base de datos";
+        }else{
+            //echo "Conexión exitosa";
+        }
+    }
+    // para retornar o traer informacion
+    //funcion para retornar datos
+    public function retornarDatos($consulta){
+        try {
+           $datos = $this->con->query($consulta);
+           return $datos;
+        } catch (Exception $ex) {
+            echo $ex->getMessage();
+        }
+    }
+    //funcion para enviar datos
+    public function enviarDatos($consulta){
+        try {
+            $enviar = $this->con->query($consulta);
+            if($enviar){
+                return true;
+            }else{
+                return false;
+            }
+        } catch (Exception $ex) {
+            echo $ex->getMessage();
+        }
     }
 }
 ?>
